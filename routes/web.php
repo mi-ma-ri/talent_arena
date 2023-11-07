95f1f7c44519137901e4df0a707131e0c961ccb3
mi-ma-ri <98933652+mi-ma-ri@users.noreply.github.com>
2023-11-07 21:43:22 +0900

    Revert "ログイン・新規登録画面に関する作成_作業途中"

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,11 +16,3 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-// Route::view('/player_register', 'auth.player_register')->name('player_register');
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/choose_register', [App\Http\Controllers\Auth\RegisterController::class, 'choose'])->name('choose');
-Route::get('/player_register', [App\Http\Controllers\Auth\RegisterController::class, 'player'])->name('player');
