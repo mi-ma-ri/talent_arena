892af0d2cb2032f552bfac1c46a6a7d33503a4b4
michihiro.shimizu <michihiros93@gmail.com>
2023-11-15 00:03:54 +0900

    db_migrate

---
+++ database/migrations/2023_11_14_141719_players_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sports_id')->constrained('sports');
+            $table->foreignId('statuses_id')->constrained('statuses');
+            $table->timestamps();
+            $table->string('full_name', 128);
+            $table->integer('gender');
+            $table->date('birthday');
+            $table->string('email', 100)->unique();
+            $table->string('current_team', 100);
+            $table->string('position', 30);
+            $table->string('password', 1000); // ハッシュ化されたパスワードを格納するための長さ
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
---
+++ database/migrations/2023_11_14_144902_video_posts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('video_posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('players_id')->constrained('players');
+            $table->foreignId('scouts_team_id')->constrained('scouts_team');
+            $table->timestamps();
+            $table->datetime('post_date');
+            $table->string('post_url_1', 255);
+            $table->string('check_point_1', 100);
+            $table->string('post_url_2', 255);
+            $table->string('check_point_2', 100);
+            $table->string('post_url_3', 255);
+            $table->string('check_point_3', 100);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('video_posts');
+    }
+};
