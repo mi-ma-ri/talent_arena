3fb24e2e9e712b4669e5b307e0d6d087ce7bfadf
michihiro.shimizu <michihiros93@gmail.com>
2024-01-03 22:09:28 +0900

    コンフリクトで修正した部分で不具合

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\VideoPosts;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 
 class Player extends Authenticatable
